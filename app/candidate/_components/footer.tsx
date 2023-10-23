49b517e376cef01e623a1bbd7077c31a93f5fa9b
Halil <halillkesgin@gmail.com>
2023-10-23 04:13:16 +0300

    fixed bugs

--- app/candidate/_components/footer.tsx
+++ app/candidate/_components/footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
         <div className="fixed justify-between container bottom-0 py-5 z-40 flex items-center bg-transparent border-t-[2px]">
-            <div>hirego</div>
-            <div>faqs</div>
+            <div>Hirego</div>
+            <div>Your Applications</div>
         </div>
     )
 }
